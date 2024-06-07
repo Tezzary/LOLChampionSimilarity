8d041e4a5105c32141c43de224d19088004e8beb
Zach <zacharyeverett.00@gmail.com>
2024-06-07 18:53:36 +1000

    setup with every 1000 requests to do backup, 3 depth, 4 games per player, estimated time 1.7k minutes, ready for first trial on other pc

--- usernamefinder.py
+++ usernamefinder.py
@@ -2,7 +2,7 @@ from apimanager import safe_make_request
 from dotenv import load_dotenv
 import os
 from sendtoresults import write_to_json
-
+from time import time
 load_dotenv()
 
 initial_puuid = os.getenv("INITIAL_PUUID")
@@ -10,7 +10,14 @@ initial_puuid = os.getenv("INITIAL_PUUID")
 matches_per_puuid_url = "https://sea.api.riotgames.com/lol/match/v5/matches/by-puuid/"
 
 depth = 3
-games_per_player = 1
+games_per_player = 4
+
+requests_per_minute = 50 # Riot API rate limit
+
+#honestly not sure if these calculations actually make sense/are correct, but they should be close enough idk if the 11's are right.
+print("Maximum total requests: " + str((games_per_player*11) ** depth))
+print("Maximum total puuids: " + str((games_per_player*10) ** (depth + 1)))
+print("Maximum total time: " + str((games_per_player*11) ** depth / requests_per_minute) + " minutes")
 
 puuids = [[] for i in range(depth + 1)]
 puuids[0].append(initial_puuid)
@@ -31,20 +38,41 @@ def get_match_details(match_id):
 
 total_requests_made = 0
 
+backup_every = 1000
+def update_requests_made():
+    global total_requests_made
+    total_requests_made += 1
+    if total_requests_made % backup_every == 0:
+        write_to_json({"totalRequests": total_requests_made, "totalCount": totalCount, "countPerDepth": countPerDepth, "puuids": puuids}, f"puuids_backup_{total_requests_made}.json")
+    print(f"Total requests made: {total_requests_made}", end="\r")
+
+start_time = time()
+
 for i in range(depth):
     for puuid in puuids[i]:
+        
         match_ids = get_recent_match_ids(puuid)
-        total_requests_made += 1
-        print(f"Total requests made: {total_requests_made}")
+        update_requests_made()
+
         for match_id in match_ids:
             if match_ids_used in match_ids:
                 continue
             match_ids_used.append(match_id)
+
             details = get_match_details(match_id)
-            total_requests_made += 1
-            print(f"Total requests made: {total_requests_made}")
+            update_requests_made()
+
             for participant in details["info"]["participants"]:
                 if not puuid_already_used(participant["puuid"]):
                     puuids[i + 1].append(participant["puuid"]) 
 
-write_to_json({"puuids": puuids}, "puuids.json")
+countPerDepth = [len(puuids[i]) for i in range(depth + 1)]
+totalCount = sum(countPerDepth)
+print("")
+print("Total count: " + str(totalCount))
+print("Count per depth: " + str(countPerDepth))
+
+print("Time taken: " + str((time() - start_time) / 60) + " minutes")
+print("Total requests made: " + str(total_requests_made))
+
+write_to_json({"totalRequests": total_requests_made, "totalCount": totalCount, "countPerDepth": countPerDepth, "puuids": puuids}, "puuids.json")
